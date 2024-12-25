76f9216a81d6f95cfbe133e7e0c8fd5d195cbb7a
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2024-12-25 15:43:32 +0700

    refactor and create crud Category

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const methodOverride = require('method-override');
+
+// Mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/db_staycation');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -15,6 +20,8 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(methodOverride('_method'));
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/admin.js
+++ routes/admin.js
@@ -3,4 +3,19 @@ const adminController = require('../controller/adminController');
 
 router.get('/dashboard', adminController.viewDashboard);
 
+// Category
+router.get('/category', adminController.viewCategory);
+router.post('/category', adminController.addCategory);
+router.put('/category', adminController.editCategory);
+router.delete('/category/:id', adminController.deleteCategory);
+
+// Bank
+router.get('/bank', adminController.viewBank);
+
+// Item
+router.get('/item', adminController.viewItem);
+
+// Booking
+router.get('/booking', adminController.viewBooking);
+
 module.exports = router;
