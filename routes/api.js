db4f59dc845db08f9e28339888c92470c984d5ab
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2024-12-30 16:49:26 +0700

    done create api booking

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const apiController = require("../controller/apiController");
-const adminController = require('../controller/apiController');
-// const { uploadSingle, uploadMultiple } = require('../middlewares/multer');
+const { uploadSingle } = require('../middlewares/multer');
 
 router.get('/landing-page', apiController.landingPage);
 router.get('/detail-page/:id', apiController.detailPage);
+router.post('/booking-page', uploadSingle, apiController.bookingPage);
 
 
 module.exports = router;
